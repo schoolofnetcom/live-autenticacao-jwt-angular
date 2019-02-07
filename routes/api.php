fd7c30862fd30fcd68a0ea4fd3efe0e514eb646f
Luiz <argentinaluiz@gmail.com>
2019-02-07 09:53:11 -0200

    Disable auto-refreshtoken

--- routes/api.php
+++ routes/api.php
@@ -25,14 +25,14 @@ Route::group(['namespace' => 'Api'], function(){
     Route::group(['middleware' => ['auth:api']], function () {
         Route::post('logout', 'AuthController@logout');
 
-        Route::group(['middleware' => 'jwt.refresh'], function () {
+        //Route::group(['middleware' => 'jwt.refresh'], function () {
 //        Route::get('users', function () {
 //            return \App\Models\User::all();
 //        });
             Route::get('categories/{category}/bill_pays', 'CategoryBillPayController@index');
             Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
             Route::resource('bill_pays', 'BillPayController', ['except' => ['create', 'edit']]);
-        });
+        //});
 
     });
 });
